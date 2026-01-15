e2ae3df747291930a5328375734aa408070b9314
glycin <alex.chatzizacharias@gmail.com>
2026-01-15 18:10:19 +0100

    made credits sleeker

--- src/main/kotlin/com/glycin/intelli25/ui/CreditsDialog.kt
+++ src/main/kotlin/com/glycin/intelli25/ui/CreditsDialog.kt
@@ -1,9 +1,11 @@
 package com.glycin.intelli25.ui
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.JBUI
+import java.awt.BorderLayout
 import java.awt.Component
 import java.awt.Dimension
 import java.awt.Font
@@ -14,41 +16,47 @@ import javax.swing.JComponent
 import javax.swing.JPanel
 
 class CreditsDialog(project: Project?) : DialogWrapper(project) {
-
     init {
         title = "Credits"
         init()
     }
 
     override fun createCenterPanel(): JComponent {
-        val panel = JPanel()
-        panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
-        val font = Fonts.jbMono.deriveFont(16f)
+        val rootPanel = JPanel(BorderLayout(JBUI.scale(20), 0))
 
-        panel.add(createLabel("This game was created by Alexander Chatzizacharias", font))
+        val iconLabel = JBLabel(AllIcons.General.InformationDialog)
+        val iconPanel = Box.createVerticalBox()
+        iconPanel.add(iconLabel)
+        iconPanel.add(Box.createVerticalGlue())
+        rootPanel.add(iconPanel, BorderLayout.WEST)
 
-        panel.add(Box.createVerticalStrut(JBUI.scale(15)))
+        val contentPanel = JPanel()
+        contentPanel.layout = BoxLayout(contentPanel, BoxLayout.Y_AXIS)
 
-        panel.add(createLabel("In collaboration with:", font))
-        panel.add(createLabel("     Irina Mariasova – IntelliJ IDEA Marketing", font))
-        panel.add(createLabel("     Andrei Kogun – Developer Consultant", font))
-        panel.add(createLabel("     Arina Kovrizhkina, FOREAL® studio – Design", font))
+        val standardFont = Fonts.jbMono.deriveFont(14f)
 
-        panel.add(Box.createVerticalStrut(JBUI.scale(15)))
+        contentPanel.add(createLabel("This game was created by Alexander Chatzizacharias (https://github.com/glycin)", standardFont))
 
-        panel.add(createLabel("Sponsored by JetBrains s.r.o. for IntelliJ IDEA's 25th birthday.", font))
+        contentPanel.add(Box.createVerticalStrut(JBUI.scale(15)))
+        contentPanel.add(createLabel("In collaboration with:", standardFont))
+        contentPanel.add(createLabel("- Irina Mariasova (IntelliJ IDEA Marketing)", standardFont))
+        contentPanel.add(createLabel("- Andrei Kogun (Developer Consultant)", standardFont))
+        contentPanel.add(createLabel("- Arina Kovrizhkina, FOREAL® studio (Design)", standardFont))
 
-        panel.preferredSize = Dimension(650, 200)
+        contentPanel.add(Box.createVerticalStrut(JBUI.scale(15)))
+        contentPanel.add(createLabel("Sponsored by JetBrains s.r.o. for IntelliJ IDEA's 25th birthday.", standardFont))
 
-        return panel
+        rootPanel.add(contentPanel, BorderLayout.CENTER)
+        rootPanel.preferredSize = Dimension(600, 150)
+
+        return rootPanel
     }
 
     override fun createActions(): Array<Action> {
         return arrayOf(okAction)
     }
 
-
-    private fun createLabel(text: String, font: Font): JBLabel {
+    fun createLabel(text: String, font: Font): JBLabel {
         return JBLabel(text).apply {
             this.font = font
             alignmentX = Component.LEFT_ALIGNMENT
