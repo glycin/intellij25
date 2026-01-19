3eab7e2702e19abfa191e7f070f46462d6b3ff74
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2026-01-19 18:26:46 +0100

    [toolwindow] add basic logging to the toolwindow factory

--- src/main/kotlin/com/glycin/intelli25/ui/GameToolWindowFactory.kt
+++ src/main/kotlin/com/glycin/intelli25/ui/GameToolWindowFactory.kt
@@ -2,31 +2,34 @@ package com.glycin.intelli25.ui
 
 import com.glycin.intelli25.persistence.GameSaveState
 import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
 
 class GameToolWindowFactory : ToolWindowFactory, DumbAware {
 
-    private var currentContent: Content? = null
-    private var gameScreenContainer: ToolWindowBaseComponent? = null
+    private val Log = Logger.getInstance(this::class.java)
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val gameState = service<GameSaveState>()
-
-        if (gameScreenContainer == null) {
-            gameScreenContainer = ToolWindowBaseComponent(project, toolWindow, gameState)
-        }
+        val gameScreenContainer = ToolWindowBaseComponent(project, toolWindow, gameState)
         val contentFactory = ContentFactory.getInstance()
+        val content = contentFactory.createContent(gameScreenContainer, null, false)
 
-        if (currentContent == null) {
-            currentContent = contentFactory.createContent(gameScreenContainer, null, false)
-            toolWindow.contentManager.addContent(currentContent!!)
-        }
+        toolWindow.contentManager.addContent(content)
+        gameScreenContainer.showScreen()
 
-        gameScreenContainer?.showScreen()
+        Log.info(
+            """### 
+              |### Game tool window created with state:
+              |### Dialogues seen: ${gameState.dialoguesSeen}
+              |### Enemies seen: ${gameState.enemiesSeen}
+              |### Levels beaten: ${gameState.levelsBeaten}
+              |###
+              |""".trimMargin()
+        )
     }
 }
