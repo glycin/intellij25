51365610dbec45853d81de9ad16161b1fd33cafd
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2026-01-19 18:02:37 +0100

    [toolwindow] make the tool window DumAware - don't require indexes to show it (fixes #1)

--- src/main/kotlin/com/glycin/intelli25/ui/GameToolWindowFactory.kt
+++ src/main/kotlin/com/glycin/intelli25/ui/GameToolWindowFactory.kt
@@ -2,13 +2,14 @@ package com.glycin.intelli25.ui
 
 import com.glycin.intelli25.persistence.GameSaveState
 import com.intellij.openapi.components.service
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
 
-class GameToolWindowFactory : ToolWindowFactory {
+class GameToolWindowFactory : ToolWindowFactory, DumbAware {
 
     private var currentContent: Content? = null
     private var gameScreenContainer: ToolWindowBaseComponent? = null
